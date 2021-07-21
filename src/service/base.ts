a12a9170ac37e95f8723a3d8c644901803a132d9
Sin-Cheng Chen <sin-chengchen@Sin-ChengdeMacBook-Pro.local>
2021-07-21 10:19:36 +0800

    [why] refactor [what] calculate balance directly

--- src/service/base.ts
+++ src/service/base.ts
@@ -38,21 +38,22 @@ export class Amount {
     private readonly depositAmount: number = 0,
     private readonly withdrawAmount: number = 0
   ) {
-
+    this._calculateBalanceAmount();
   }
 
   public get balanceAmount(): number {
     return this._balanceAmount;
   }
 
-  public calculateBalanceAmount() {
+  public get isWithdrawAmountLessThanBalance() {
+    return new BigNumber(this.withdrawAmount).isGreaterThan(new BigNumber(this.initBalanceAmount));
+  }
+
+
+  private _calculateBalanceAmount() {
     this._balanceAmount = new BigNumber(this.initBalanceAmount)
       .plus(new BigNumber(this.depositAmount))
       .minus(new BigNumber(this.withdrawAmount))
       .toNumber();
-  }
-
-  public get isWithdrawAmountLessThanBalance() {
-    return new BigNumber(this.withdrawAmount).isGreaterThan(new BigNumber(this.initBalanceAmount));
   }
 }
